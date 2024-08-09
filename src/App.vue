cc226620f0ebfa8c5364791df08c58ef21d3ad34
takakou32 <runtothehill32@gmail.com>
2024-08-09 13:23:02 +0900

    初期ファイル配置

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container mx-auto p-4">
+      <Login v-if="!user" @login="handleLogin" />
+      <MonthlyAttendance v-else :user="user" @logout="handleLogout" />
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  import Login from './components/Login.vue'
+  import MonthlyAttendance from './components/MonthlyAttendance.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      Login,
+      MonthlyAttendance
+    },
+    setup() {
+      const user = ref(null)
+  
+      const handleLogin = (username) => {
+        user.value = username
+      }
+  
+      const handleLogout = () => {
+        user.value = null
+      }
+  
+      return {
+        user,
+        handleLogin,
+        handleLogout
+      }
+    }
+  }
+  </script>
+  
